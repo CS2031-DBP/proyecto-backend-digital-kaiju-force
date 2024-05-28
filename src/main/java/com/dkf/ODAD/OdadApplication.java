3432b77f17690e1fe901f66e629463bced1b2d2e
jucada11 <juan.paredes@utec.edu.pe>
2024-05-28 17:40:42 -0500

    update files

--- src/main/java/com/dkf/ODAD/OdadApplication.java
+++ src/main/java/com/dkf/ODAD/OdadApplication.java
@@ -1,13 +1,37 @@
 package com.dkf.ODAD;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class OdadApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OdadApplication.class, args);
+	}
+
+	@Bean
+	ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
+
+	@Bean
+	public WebMvcConfigurer corsMappingConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "PATCH")
+						.maxAge(3600)
+						.allowedHeaders("*")
+						.allowCredentials(false);
+			}
+		};
 	}
 
 }
--- src/main/java/com/dkf/ODAD/config/Util.java
+++ src/main/java/com/dkf/ODAD/config/Util.java
@@ -5,7 +5,9 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Util {
 
     @Bean

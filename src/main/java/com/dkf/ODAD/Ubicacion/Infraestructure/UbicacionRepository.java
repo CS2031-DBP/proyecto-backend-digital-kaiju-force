35abebe1a3589577a8c44725e6de7206138c0dd8
jucada11 <juan.paredes@utec.edu.pe>
2024-07-15 22:18:11 -0500

    update 2

--- src/main/java/com/dkf/ODAD/Ubicacion/Infraestructure/UbicacionRepository.java
+++ src/main/java/com/dkf/ODAD/Ubicacion/Infraestructure/UbicacionRepository.java
@@ -1,7 +1,11 @@
 package com.dkf.ODAD.Ubicacion.Infraestructure;
 
+import com.dkf.ODAD.Tratamiento.Domain.Tratamiento;
 import com.dkf.ODAD.Ubicacion.Domain.Ubicacion;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UbicacionRepository extends JpaRepository<Ubicacion, Long> {
+    Ubicacion findByUser_Id(Long pacienteId);
 }
